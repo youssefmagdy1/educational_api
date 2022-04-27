b8ece80e212af3afd13a1fc012aa01253bc7ab87
ahmedsay3d <a7m3dsay3d@gmail.com>
2022-04-27 07:47:30 +0200

    fixed staff.route

--- server/src/routes/staff.route.js
+++ server/src/routes/staff.route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express') ;
 
 const {
@@ -17,24 +16,4 @@ staffRouter.post("/staff", createStaff);
 staffRouter.put("/staff/:id", updateStaff);
 staffRouter.delete("/staff/:id", deleteStaff);
 
-=======
-const express = require('express') ;
-
-const {
-    _getAllMembers,
-    _getById,
-    _createStaffEntry,
-    _updateStaffEntry,
-    _deleteStaffEntry,
-} = require('../controller/staff.controller');
-
-const staffRouter = express.Router();
-
-staffRouter.get('/', _getAllMembers);
-staffRouter.get('/:id', _getById);
-staffRouter.post('/', _createStaffEntry);
-staffRouter.put("/:id", _updateStaffEntry);
-staffRouter.delete("/:id", _deleteStaffEntry);
-
->>>>>>> 72da2dc7047e4bf2569329cf0d2c37f70ecd83bb
 module.exports = staffRouter;
