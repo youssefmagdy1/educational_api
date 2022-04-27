2d6b2c4c875c2384f43470e9bd873f3db3bcf660
ahmedsay3d <a7m3dsay3d@gmail.com>
2022-04-27 07:38:51 +0200

    added staff endpoint with mongo support

--- server/src/routes/staff.route.js
+++ server/src/routes/staff.route.js
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+const express = require('express') ;
+
+const {
+    getAllStaff,
+    getById,
+    updateStaff,
+    createStaff,
+    deleteStaff,
+} = require('../controller/staff.controller');
+
+const staffRouter = express.Router();
+
+staffRouter.get('/staff', getAllStaff);
+staffRouter.get('/staff/:id', getById);
+staffRouter.post("/staff", createStaff);
+staffRouter.put("/staff/:id", updateStaff);
+staffRouter.delete("/staff/:id", deleteStaff);
+
+=======
 const express = require('express') ;
 
 const {
@@ -16,4 +36,5 @@ staffRouter.post('/', _createStaffEntry);
 staffRouter.put("/:id", _updateStaffEntry);
 staffRouter.delete("/:id", _deleteStaffEntry);
 
+>>>>>>> 72da2dc7047e4bf2569329cf0d2c37f70ecd83bb
 module.exports = staffRouter;
